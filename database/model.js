da91041a0216f6504bda60bbd988ed53bb665452
miahbates <miahbates@icloud.com>
2022-01-20 13:56:00 +0000

    stack-overflow comment added about adding to different tables in sql

--- database/model.js
+++ database/model.js
@@ -4,4 +4,13 @@ const db = require("./connection.js");
 function sendFacts(name, cohort, facts) {
   // *SQL*
 
+  // stack overflow to enter multiple things.
+
+// BEGIN;
+// INSERT INTO users (username, password)
+//   VALUES('test', 'test');
+// INSERT INTO profiles (userid, bio, homepage) 
+//   VALUES(LAST_INSERT_ID(),'Hello world!', 'http://www.stackoverflow.com');
+// COMMIT;
+
 }

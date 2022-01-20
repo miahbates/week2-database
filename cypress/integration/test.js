bafb3158134da6ee3bfdbca88ef312bb085d8dd6
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2022-01-20 12:38:58 +0000

    read from database and display

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -2,39 +2,37 @@ beforeEach(() => {
   cy.task("resetDb");
 });
 
-
 // test for landing on homepage
 describe("homepage tests", () => {
-it("can find homepage", () => {
-  cy.visit("/");
+  it("can find homepage", () => {
+    cy.visit("/");
+  });
+
+  // check title of landing page
+  it("can find title on home page", () => {
+    cy.visit("/");
+
+    cy.get("h1").contains("Who-said-that");
+  });
+
+  //check title of cohort facts page
+  it("can find title on facts page", () => {
+    cy.visit("/display_facts");
+    cy.get("h1").contains("Cohort facts");
+  });
+
+  it("can fill out username, personal fact and cohort (from drop down)", () => {
+    cy.visit("/");
+    cy.get("form").find("input[name='first_name']").type("Gregor");
+    cy.get("form")
+      .find("textarea[name='facts']")
+      .type("I once won a tea drinking competition");
+    cy.get("form").find("input[type='submit']").click();
+    cy.url().should("include", "/");
+  });
+
+  it("can view facts about other people", () => {
+    cy.visit("/display_facts");
+    cy.get("body").find("li").contains("Gregor");
+  });
 });
-
-// check title of landing page
-it("can find title on home page", () => {
-  cy.visit("/");
-
-  cy.get("h1").contains("Who-said-that");
-});
-
-//check title of cohort facts page
-it("can find title on facts page", () => {
-  cy.visit("/display_facts");
-  cy.get("h1").contains("Cohort facts");
-});
-
-it("can fill out username, personal fact and cohort (from drop down)", () => {
-  cy.visit("/");
-  cy.get("form").find("input[name='first_name']").type("Gregor");
-  cy.get("form")
-    .find("textarea[name='facts']")
-    .type("I once won a tea drinking competition");
-  cy.get("form").find("input[type='submit']").click();
-  cy.url().should("include", "/");
-});
-
-
-it("can view facts about other people", () => {
-  cy.visit("/display_facts");
-  cy.get("body").find("li").contains("Gregor");
-});
-
--- server.js
+++ server.js
@@ -20,49 +20,36 @@ const fakeUsers = [
 ];
 
 server.get("/display_facts", (request, response) => {
-  // const select_posts = /*sql*/ `
-  //     SELECT blog_posts.text_content, users.username
-  //     FROM blog_posts INNER JOIN users
-  //     ON blog_posts.user_id = users.id
-  //     ORDER BY blog_posts.id DESC
-  //   `;
-  // db.query(select_posts).then((result) => {
-  //   const posts = result.rows;
-  //   const postItems = posts.map((post) => {
-  //     return /*html*/ `
-  //         <li>
-  //          <p>${post.text_content}</p>
-  //          <p>${post.username}</p>
-  //         </li>
-  //       `;
-  //   });
-  //   response.send(`<ul>${postItems.join("")}</ul>`);
-  // });
+  const select_userInfo = /*sql*/ `
+    SELECT facts, first_name 
+    FROM user_facts INNER JOIN users 
+    ON user_facts.user_id = users.id;
+  `;
 
-  console.log(db);
+  db.query(select_userInfo).then((result) => {
+    let list = "";
+    const dbResponse = result.rows;
+    dbResponse.forEach((user) => {
+      list += `<li>${user.first_name}: ${user.facts}</li>`;
+    });
 
-  let list = "";
+    const html = `
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+        <h1>Cohort facts</h1>
+        <ul>${list}</ul>
+      </body>
+      </html>`;
 
-  fakeUsers.forEach((user) => {
-    list += `<li>${user.firstname}: ${user.cohort} - ${user.fact} </li>`;
+    response.send(html);
   });
-
-  const html = `
-  <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1>Cohort facts</h1>
-      <ul>${list}</ul>
-    </body>
-    </html>`;
-
-  response.send(html);
 });
 
 const PORT = 3333;
